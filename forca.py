e5f1b8c7c0f99c3c60a4aae0186c43973ab9e1df
Maike Mota <maikemota@live.com>
2021-09-08 21:52:16 -0300

    mrm

--- forca.py
+++ forca.py
@@ -4,21 +4,27 @@ def jogar():
     print("###################################")
 
     palavra_secreta = 'banana'
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
 
     while not enforcou and not acertou:
-
         chute = input('Qual a letra? ')
         chute = chute.strip()
+
 
         index = 0 
         for letra in palavra_secreta:
             if chute.lower() == letra.lower():
+                letras_acertadas[index] = letra
                 print(f"Encontrei a letra {chute} na posição {index}")
+            if ' '.join(letras_acertadas) == palavra_secreta:
+                print('Palavra correta')
+                break
             index += 1
-
+        print(letras_acertadas)
     print('Fim do jogo')
     
 if(__name__ == "__main__"):
